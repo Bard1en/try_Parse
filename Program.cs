a82f73faff52186b41e8a58b53b6269fb94f7db6
Bard1en <greger.grym@gmail.com>
2024-10-22 16:14:32 +0200

    tryparse metod

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int resultat;
+if(!TryParse(Console.ReadLine(),out resultat))
+    Console.WriteLine("skriv in en siffra");
+
+static bool TryParse(string s, out int result){
+    
+    try{
+        result = int.Parse(s);
+        return true;
+    }
+    catch{
+        result = 0;
+        return false;
+    }
+    
+    
+}
